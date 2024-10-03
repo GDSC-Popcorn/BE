a3a6ad64edad57c2ce06dadde9a2eaab9b35df27
minit88 <chymh32@gmail.com>
2024-10-03 20:44:29 +0900

    Feat: 채팅 메세지 엔터티 생성

--- src/main/java/com/popcorn/popcorn/chat/entity/Message.java
+++ src/main/java/com/popcorn/popcorn/chat/entity/Message.java
@@ -1,4 +1,17 @@
 package com.popcorn.popcorn.chat.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String text;
+    private String sender;
+    private String receiver;
 }
