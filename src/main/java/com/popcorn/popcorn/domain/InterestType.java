d7347c3cdf7b49eed1359259966cfefc94a892dc
jeongminkang <wjdals980424@naver.com>
2024-11-15 14:12:17 +0900

    feat: 회원가입 시 관심사 받기 추가

---
+++ src/main/java/com/popcorn/popcorn/domain/InterestType.java
@@ -0,0 +1,15 @@
+package com.popcorn.popcorn.domain;
+
+public enum InterestType {
+    FASHION,
+    BEAUTY,
+    FOOD,
+    CHARACTERS,
+    MOVIES,
+    LIFESTYLE,
+    ART,
+    IT,
+    SPORTS,
+    CELEBRITY,
+    PETS
+}
--- src/main/java/com/popcorn/popcorn/service/UserService.java
+++ src/main/java/com/popcorn/popcorn/service/UserService.java
@@ -1,9 +1,12 @@
 package com.popcorn.popcorn.service;
 
+import com.popcorn.popcorn.domain.InterestType;
 import com.popcorn.popcorn.domain.Role;
 import com.popcorn.popcorn.domain.dto.FirstSignupDto;
 import com.popcorn.popcorn.domain.dto.SecondSignupDto;
 import com.popcorn.popcorn.domain.entity.UserEntity;
+import com.popcorn.popcorn.domain.entity.UserInterest;
+import com.popcorn.popcorn.repository.UserInterestRepository;
 import com.popcorn.popcorn.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,6 +22,7 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final UserInterestRepository userInterestRepository;
 
     public void signup(FirstSignupDto firstSignupDto, SecondSignupDto secondSignupDto) {
         boolean isUser = userRepository.existsByUsername(firstSignupDto.getUsername());
@@ -34,7 +38,15 @@ public class UserService {
                 .name(firstSignupDto.getName())
                 .role(Role.USER)
                 .build();
+
         userRepository.save(user);
+
+        for(InterestType interest : secondSignupDto.getInterests()){
+            UserInterest userInterest = new UserInterest();
+            userInterest.setUserAndInterest(user, interest);
+            user.addUserInterest(userInterest);
+            userInterestRepository.save(userInterest);
+        }
 
     }
 }
