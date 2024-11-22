cff33046c4b3b62c099ee58885cb42d3542fbb41
jeongminkang <wjdals980424@naver.com>
2024-11-23 01:18:11 +0900

    feat: email관련 dto

---
+++ src/main/java/com/popcorn/popcorn/domain/dto/EmailRequestDto.java
@@ -0,0 +1,16 @@
+package com.popcorn.popcorn.domain.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmailRequestDto {
+
+    @Email
+    @NotBlank
+    private String email;
+
+}
