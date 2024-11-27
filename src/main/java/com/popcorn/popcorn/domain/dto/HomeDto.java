d640184a13747e926ebab64e1e9416406cb01f69
이채은 Lee Chae-Eun <ioo22@pukyong.ac.kr>
2024-11-27 17:36:48 +0900

    feat: 팝업 상세 화면 구현

--- src/main/java/com/popcorn/popcorn/domain/dto/HomeDto.java
+++ src/main/java/com/popcorn/popcorn/domain/dto/HomeDto.java
@@ -1,15 +1,19 @@
 package com.popcorn.popcorn.domain.dto;
 
+import lombok. *;
+
+@Getter
 public class HomeDto {
+    private Long popupId;
     private String title;
+    private String popupImage;
+    private int dDay;
 
-    public HomeDto(String title) {
+    public HomeDto(Long popupId, String title, String popupImage, int dDay) {
+        this.popupId = popupId;
         this.title = title;
-    }
-
-    //Getters
-    public String getTitle() {
-        return title;
+        this.popupImage = popupImage;
+        this.dDay = dDay;
     }
 }
 
--- src/main/java/com/popcorn/popcorn/repository/PopupRepository.java
+++ src/main/java/com/popcorn/popcorn/repository/PopupRepository.java
@@ -2,7 +2,14 @@ package com.popcorn.popcorn.repository;
 
 import com.popcorn.popcorn.domain.entity.PopupEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
 
+@Repository
 public interface PopupRepository extends JpaRepository <PopupEntity, Long>{
+    //종료일 오름차순으로 팝업 목록 정렬
+    List<PopupEntity> findAllByOrderByEnded_atAsc();
+    //팝업 ID로 팝업 조회
+    PopupEntity findByPopupId(Long popupId);
 }
