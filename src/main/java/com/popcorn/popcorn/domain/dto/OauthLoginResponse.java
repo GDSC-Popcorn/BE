0aeb610798c75393a0e0116197b402950bd50407
jeongminkang <wjdals980424@naver.com>
2025-02-08 21:03:53 +0900

    chore: 필요없는 주석 제거

--- src/main/java/com/popcorn/popcorn/domain/dto/OauthLoginResponse.java
+++ src/main/java/com/popcorn/popcorn/domain/dto/OauthLoginResponse.java
@@ -3,7 +3,6 @@ package com.popcorn.popcorn.domain.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Builder
