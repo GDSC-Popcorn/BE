a3146538a597b0f5fe88102e154ff252464d0a6f
ioo5959 <ioo22@pukyong.ac.kr>
2025-04-07 11:32:29 +0900

    fix: 로직 변경 후 필요없는 변수 삭제

--- src/main/java/com/popcorn/popcorn/service/PopupService.java
+++ src/main/java/com/popcorn/popcorn/service/PopupService.java
@@ -165,8 +165,6 @@ public class PopupService {
         //찜 여부 확인
         boolean isLiked = false;
         if(userId != null){ //userId가 있는 경우에만 찜 여부 확인
-            UserEntity user = userRepository.findById(userId)
-                    .orElseThrow(()->new RuntimeException("User not found ID:" + userId));
             isLiked = userLikeRepository.existsByUserIdAndPopupId(userId,popupId);
         }
 
