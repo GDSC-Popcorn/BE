465526e249d7fb60f33fd15e81139de5951c18d5
jeongminkang <wjdals980424@naver.com>
2025-06-07 01:08:12 +0900

    feat(exception): 오류, 예외 추가

--- src/main/java/com/popcorn/popcorn/common/error/ErrorCode.java
+++ src/main/java/com/popcorn/popcorn/common/error/ErrorCode.java
@@ -15,7 +15,12 @@ public enum ErrorCode implements ErrorCodeIfs{
     NOT_ACCESS("fail", 403, "엑세스 할 수 없음"),
 
     TOKEN_EXPIRED("fail", 401, "만료된 토큰"),
-    INVALID_TOKEN("fail", 401,"잘못된 토큰, 재 로그인해주세요");
+    INVALID_TOKEN("fail", 401,"잘못된 토큰, 재 로그인해주세요"),
+
+    POPUP_NOT_FOUND("fail", 202, "존재하지 않는 팝업"),
+    REVIEW_NOT_FOUND("fail", 202, "존재하지 않는 리뷰"),
+    S3_UPLOAD_FAILED("fail", 500, "s3에 업로드 실패"),
+    REVIEW_NOT_MATCH("fail", 205, "리뷰 글쓴이가 일치 하지 않음");
 
     private final String status;
     private final Integer resultCode;
